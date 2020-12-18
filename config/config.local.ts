16dbf259560880f6edd4c18e14104c7e7281a2a7
lixinxiang <lixinxiang@vv.cn>
2020-12-18 23:32:04 +0800

    init

---
+++ config/config.local.ts
@@ -0,0 +1,14 @@
+import { EggAppConfig, PowerPartial } from "egg";
+
+export default () => {
+  const config: PowerPartial<EggAppConfig> = {};
+  config.sequelize = {
+    dialect: "mysql",
+    host: "127.0.0.1",
+    username: "root",
+    password: "12345678",
+    port: 3306,
+    database: "oa",
+  };
+  return config;
+};
